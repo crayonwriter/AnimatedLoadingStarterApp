2475f3872cccc8fe0799e246751ebe7b3c56a660
Theda Rogers <crayonwriter@gmail.com>
2021-05-20 08:27:16 -0400

    Rename radiobutton IDs

--- src/main/java/com/udacity/MainActivity.kt
+++ src/main/java/com/udacity/MainActivity.kt
@@ -9,6 +9,8 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
